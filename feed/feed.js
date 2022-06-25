a8fc5da825c2f0008640ed01d9af5b781e48c2a3
aline-guttner <aline.guttner@gmail.com>
2022-06-25 00:08:18 -0300

    commit on 'master'

--- feed/feed.js
+++ feed/feed.js
@@ -58,11 +58,58 @@ function mostraformulario() { // mostra formulário de postagem
     chamaForm.style.display = "none" //oculta a div chama form
     formPostagem.style.display = "block" // exibe a div do formulário
 
+
+
 }
+
+let formImagem = document.getElementById("imagem")
+formImagem.addEventListener('change', function () {
+
+    const chosenFile = this.files[0];
+
+    const reader = new FileReader();
+    reader.readAsDataURL(chosenFile);
+    if (chosenFile) {
+        reader.addEventListener('load', function () {
+
+            let imgDiv = document.createElement('div')
+            imgDiv.classList.add('imgFormDiv')
+
+            let img = document.createElement('img')
+            img.setAttribute('src', reader.result);
+            img.classList.add('imgFormPostagem')
+            let newid = Math.floor(Math.random() * 10000000)
+            img.setAttribute('id', newid)
+
+            let imgXis = document.createElement('img')
+            imgXis.setAttribute('src', '../perfil/x-mark-16.png')
+            imgXis.classList.add('imgXis')
+            imgXis.setAttribute('alt', 'Símbolo em formato de X')
+            imgXis.setAttribute('aria-label', 'Clicar no X exclui a imagem')
+            imgXis.addEventListener('click', function (e) {
+                this.parentNode.remove()
+            })
+
+            imgDiv.append(img, imgXis)
+            document.getElementById('divImagemPostar').appendChild(imgDiv)
+
+
+        });
+
+    }
+
+})
+
+
+
 function insertAfter(newNode, existingNode) {
     existingNode.parentNode.insertBefore(newNode, existingNode.nextSibling);
 }
 function postar() {
+    let inputImagem = document.getElementById('imagem')
+
+
+
     let chamaForm = document.getElementById("chama-form") // pega a div chama form
     let formPostagem = document.getElementById("form-postagem") // pega a div form postagem
     chamaForm.style.display = "block" // exibe a div que chama o formulário
@@ -70,7 +117,7 @@ function postar() {
 
     let formTitulo = document.getElementById("titulo").value // pegando os valores do form
     let formConteudo = document.getElementById("conteudo").value
-    let formImagem = document.getElementById("imagem").value
+    let formImagens = document.getElementsByClassName("imgFormPostagem")
 
     let postagens = document.getElementById("area-postagens") // pegando a área onde vai ficar a postagem
     let postagem = document.createElement("div") // criar div para postagem
@@ -130,15 +177,88 @@ function postar() {
     dataPost.innerHTML = `postado em ${data.getDate()} de ${mes} de ${data.getFullYear()}`
     postagem.appendChild(dataPost)
 
-    let imagemPost = document.createElement("img") // criar elemento img
-    imagemPost.style.width = "620px"
-    imagemPost.src = formImagem
-    postagem.appendChild(imagemPost)
+
+
+    if (formImagens.length === 0) {
+        return
+    } else if (formImagens.length === 1) {
+
+        let imagemPost = document.createElement("img") // criar elemento img
+        imagemPost.classList.add('img-fluid')
+        imagemPost.src = formImagens[0].src
+        postagem.appendChild(imagemPost)
+    }
+    else {
+
+        let Carousel = document.createElement('div')
+
+        Carousel.setAttribute('id', 'carouselExampleControls')
+        Carousel.classList.add('carousel', 'slide')
+        Carousel.setAttribute('data-bs-ride', 'carousel')
+
+        let carouselInner = document.createElement('div')
+        carouselInner.classList.add('carousel-inner')
+
+        for (i = 0; i < formImagens.length; i++) {
+
+            let carouselItem = document.createElement('div')
+            carouselItem.classList.add('carousel-item')
+
+            let carouselImg = document.createElement('img')
+            carouselImg.classList.add('img-fluid', 'd-block', 'imagemPostadaCarousel')
+            carouselImg.src = formImagens[i].src
+
+
+            carouselItem.appendChild(carouselImg)
+            carouselInner.appendChild(carouselItem)
+        }
+        carouselInner.firstChild.classList.add('active')
+
+
+        let controlPrev = document.createElement('button')
+        controlPrev.classList.add('carousel-control-prev')
+        controlPrev.setAttribute('data-bs-target', `#carouselExampleControls`)
+        controlPrev.setAttribute('role', 'button')
+        controlPrev.setAttribute('data-bs-slide', 'prev')
+
+        let prevIcon = document.createElement('span')
+        prevIcon.classList.add('carousel-control-prev-icon', 'prevImgPostada')
+        prevIcon.setAttribute('aria-hidden', 'true')
+
+        let vHidden = document.createElement('span')
+        vHidden.classList.add('visually-hidden')
+        vHidden.innerHTML = 'Previous'
+
+        controlPrev.append(prevIcon, vHidden)
+
+        let controlNext = document.createElement('button')
+        controlNext.classList.add('carousel-control-next')
+        controlNext.setAttribute('data-bs-target', `#carouselExampleControls`)
+        controlNext.setAttribute('role', 'button')
+        controlNext.setAttribute('data-bs-slide', 'next')
+
+        let nextIcon = document.createElement('span')
+        nextIcon.classList.add('carousel-control-next-icon', 'nextImgPostada')
+        nextIcon.setAttribute('aria-hidden', 'true')
+
+        let vHidden2 = document.createElement('span')
+        vHidden2.classList.add('visually-hidden')
+        vHidden2.innerHTML = 'Next'
+
+        controlNext.append(nextIcon, vHidden2)
+
+        Carousel.append(carouselInner, controlPrev, controlNext)
+
+        postagem.appendChild(Carousel)
+
+    }
+
+
 
     let conteudo = document.createElement("p") // criar conteúdo da postagem
     conteudo.innerHTML = formConteudo // adicionou o conteúdo 
     postagem.appendChild(conteudo) // adicionou o conteúdo na postagem 
     conteudo.setAttribute("id", "conteudo")
 
-
+    $('.imgFormDiv').remove()
 }
